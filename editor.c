436192e06debc15d1a87dfc3fdd187b82b861c8e
rsa015 <s.adjignon015@hotmail.com>
2021-10-13 21:03:55 +0000

    displaying keypress

--- editor.c
+++ editor.c
@@ -1,6 +1,8 @@
 #include <unistd.h>
 #include <termios.h>
 #include <stdlib.h>
+#include <stdio.h>
+#include <ctype.h>
 
 
 struct termios orig_termios;
@@ -11,12 +13,18 @@ void disableRawMode() {
 
 
 void enableRawMode() {
+	/*
+	Enables raw mode:
+		 pass in the original value of struct at the beginning and 		    in other to reset the terminal prperly at exit
+	*/
+
 	tcgetattr(STDIN_FILENO, &orig_termios);
+	
 	atexit(disableRawMode);
 
 	struct termios raw = orig_termios;
-	raw.c_lflag &= ~(ECHO);
-
+	raw.c_lflag &= ~(ECHO | ICANON);
+	
 	tcsetattr(STDIN_FILENO, TCSAFLUSH,&raw); 
 }
 
@@ -24,6 +32,13 @@ void enableRawMode() {
 int main() {
 	enableRawMode();
 	char c;
-	while(read(STDIN_FILENO, &c, 1)==1 && c != 'q');
+	/*Reads the character in c, but exits as soon as it reads 'q'*/	
+	while(read(STDIN_FILENO, &c, 1)==1 && c != 'q'){
+		if (iscntrl(c)) {
+			printf("%d\n", c);
+		} else {	
+			printf("%d ('%c')\n", c, c);
+		}
+	}
 	return 0;
 }
