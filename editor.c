660ed7c76393c0fcc988cb9800e9693c73c95f6d
rsa015 <s.adjignon015@hotmail.com>
2021-10-14 08:10:08 +0000

    add allowable time for the read function to operate

--- editor.c
+++ editor.c
@@ -25,21 +25,26 @@ void enableRawMode() {
 	struct termios raw = orig_termios;
 	raw.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG);
 	raw.c_oflag &= ~(OPOST);
-	raw.c_iflag &= ~(IXON | ICRNL); //IXON:  Disable ctrl-S and ctrl-Q	
+	raw.c_iflag &= ~(IXON | ICRNL | BRKINT | ISTRIP | INPCK); //IXON:  Disable ctrl-S and ctrl-Q
+	raw.c_cflag |= (CS8);
+	raw.c_cc[VMIN] = 0;
+	raw.c_cc[VTIME] = 1;	
 	tcsetattr(STDIN_FILENO, TCSAFLUSH,&raw); 
 }
 
 
 int main() {
 	enableRawMode();
-	char c;
+	char c = '\0';
+	read(STDIN_FILENO, &c, 1);
 	/*Reads the character in c, but exits as soon as it reads 'q'*/	
-	while(read(STDIN_FILENO, &c, 1)==1 && c != 'q'){
+	while(1){
 		if (iscntrl(c)) {
-			printf("%d\n", c);
+			printf("%d\r\n", c);
 		} else {	
-			printf("%d ('%c')\n", c, c);
+			printf("%d ('%c')\r\n", c, c);
 		}
+		if (c = 'q') {break;}
 	}
 	return 0;
 }
