05d62e1cc98644e880666098753b32aea1b858c0
rsa015 <s.adjignon015@hotmail.com>
2021-10-25 10:40:26 +0000

    Appending to a buffer before Outputing

--- editor.c
+++ editor.c
@@ -1,11 +1,15 @@
 #include <unistd.h>
 #include <termios.h>
 #include <stdlib.h>
+#include <string.h>
 #include <stdio.h>
 #include <ctype.h>
 #include <errno.h>
 #include <sys/ioctl.h>
 
+
+#define QUASAR_VERSION "1.0.0"
+#define ABUF_INIT {NULL, 0}
 #define CTRL_KEY(k) ((k) & 0x1f)
 
 struct editorConfig {
@@ -14,6 +18,12 @@ struct editorConfig {
 	struct termios orig_termios;
 };
 
+struct abuf {
+	char *b;
+	int len;
+};
+
+
 struct editorConfig E;
 
 void die(const char *s) {
@@ -21,6 +31,20 @@ void die(const char *s) {
 	write(STDOUT_FILENO, "\x1b[H", 3);
 	perror(s);
 	exit(1);
+}
+
+void abAppend(struct abuf *ab, const char *s, int len) {
+	char *new =  realloc(ab->b, ab->len + len);
+
+	if (new == NULL) return;
+
+	memcpy(&new[ab->len], s, len);
+	ab -> b;
+	ab -> len += len;
+}
+
+void abFree(struct abuf *ab) {
+	free(ab -> b);
 }
 
 
@@ -88,17 +112,32 @@ void initEditor() {
 }
 
 
-void editorDrawsRows() {
+void editorDrawsRows(struct abuf *ab) {
 	int y;
 	for (y =0; y < E.screenrows; y++) {
-		write(STDOUT_FILENO, "~", 1);
+		if (y == E.screenrows / 3) {
+			char welcome[80];
+			int welcomelen = snprintf(welcome, sizeof(welcome), "Quasar --version %s", QUASAR_VERSION);
+			if (welcomelen > E.screencols) welcomelen = E.screencols;
+			int padding = (E.screencols - welcomelen) / 2;
+			
+			if (padding) { abAppend(ab, "~", 1); padding--;}
+
+			while (padding--) {abAppend(ab, " ", 1); }
+	
+			abAppend(ab, welcome, welcomelen);
+
+		} else { abAppend(ab, "~", 1);}
 		
+		abAppend(ab, "\x1b[K", 3);	
 		if (y < E.screenrows -1) {
-			write(STDOUT_FILENO, "\r\n", 2);
+			abAppend(ab, "\r\n", 2);
 		}	
 	}
 
 }
+
+
 void editorProcessKeypress() {
 	char c = editorReadKey();
 	
@@ -112,12 +151,20 @@ void editorProcessKeypress() {
 }
 
 void editorRefreshScreen() {
-	write(STDIN_FILENO, "\x1b[H", 3);
-	write(STDIN_FILENO, "\x1b[2J", 4);
+	struct abuf ab = ABUF_INIT;
 
-	editorDrawsRows();
+	abAppend(&ab, "\x1b[?25l", 6);
+	abAppend(&ab, "\x1b[H", 3);
 
-	write(STDIN_FILENO, "\x1b[H", 3);
+	editorDrawsRows(&ab);
+
+	abAppend(&ab, "\x1b[H", 3);
+	abAppend(&ab, "\x1b[?25h", 6);
+	
+	write(STDIN_FILENO, ab.b, ab.len);
+
+	abFree(&ab);
+
 }
 
 
