32dea6e8b2ed67b35db6e0b43f94e77701912649
rsa015 <s.adjignon015@hotmail.com>
2022-01-05 02:13:55 +0000

    Raw Input && Output

--- editor.c
+++ editor.c
@@ -13,10 +13,15 @@
 #define CTRL_KEY(k) ((k) & 0x1f)
 
 enum editorKey {
-	ARROW_DOWN = 1000,
+	ARROW_DOWN, 
 	ARROW_UP,
-	ARROW_LEFT,
+	ARROW_LEFT = 1000,
+	END_KEY,
+	HOME_KEY,
+	DEL_KEY,
 	ARROW_RIGHT,
+	PAGE_UP,
+	PAGE_DOWN,
 };
 
 struct editorConfig {
@@ -69,11 +74,33 @@ int editorReadKey() {
 		if (read(STDIN_FILENO, &seq[1], 1) != 1) return '\x1b';
 
 		if (seq[0] == '[') {
-			switch (seq[1]) {
-				case 'A': return ARROW_UP;
-				case 'B': return ARROW_DOWN;
-				case 'C': return ARROW_RIGHT;
-				case 'D': return ARROW_LEFT;
+			if (seq[1] >= '0' && seq[1] <= '9') {
+				if (read(STDIN_FILENO, &seq[2], 1)) return '\x1b';
+				if (seq[2] == '~') {
+					switch (seq[1]) {
+						case '1': return HOME_KEY;
+						case '3': return DEL_KEY;
+						case '4': return END_KEY;
+						case '5': return PAGE_UP;
+						case '6': return PAGE_DOWN;
+						case '7': return HOME_KEY;
+						case '8': return END_KEY;
+					}
+				}
+			} else {
+				switch (seq[1]) {
+					case 'A': return ARROW_UP;
+					case 'B': return ARROW_DOWN;
+					case 'C': return ARROW_RIGHT;
+					case 'D': return ARROW_LEFT;
+					case 'H': return HOME_KEY;
+					case 'F': return END_KEY;
+				}
+			}
+		} else if (seq[0] == '0') {
+			switch(seq[1]) {
+				case 'H': return HOME_KEY;
+				case 'F': return END_KEY;
 			}
 		}
 		return '\x1b';
@@ -82,46 +109,46 @@ int editorReadKey() {
 	}
 }
 
-
 int getCursorPosition(int *rows, int *cols) {
 	char buf[32];
 	unsigned int i = 0;
 
 	if (write(STDOUT_FILENO, "\x1b[6n", 4) != 4) return -1;
-	
+
 
 	while (i < sizeof(buf)-1) {
 		if (read(STDIN_FILENO, &buf[i], 1) != 1) break;
 		if (buf[i] == 'R') break;
 		i++;
 	}
-	
+
 	buf[i] = '\0';
-	
+
 	if (buf[0] != '\x1b' || buf[1] != '[') return -1;
 	if (sscanf(&buf[2], "%d;%d", rows, cols) != 2) return -1;
-	
+
 	return 0;
 }
+
 int getWindowsSize(int *rows, int *cols) {
 	struct winsize ws;
-	
+
 	if (ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws) == -1 || ws.ws_col == 0) {
 		if (write(STDOUT_FILENO, "\x1b[999C\x1b[999B", 12) != 12) {
 			return -1;	
 		}
-		
+	
 		return getCursorPosition(rows, cols);	
 	} else {
 		*cols = ws.ws_col;
 		*rows = ws.ws_row;
-		return 0;	
-	}
-
+		}
+	return 0;	
 }
+
 int getWindowSize(int *rows, int *cols) {
 	struct winsize ws;
-	
+
 	if (ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws)== -1 || ws.ws_col == 0) {
 		return -1;
 	} else {
@@ -129,7 +156,6 @@ int getWindowSize(int *rows, int *cols) {
 		*rows = ws.ws_row;
 		return 0;
 	}
-	
 }
 
 void initEditor() {
@@ -146,22 +172,22 @@ void editorDrawsRows(struct abuf *ab) {
 		if (y == E.screenrows / 3) {
 			char welcome[80];
 			int welcomelen = snprintf(welcome, sizeof(welcome), "Quasar --version %s", QUASAR_VERSION);
-			if (welcomelen > E.screencols) welcomelen = E.screencols;
+			if (welcomelen > E.screencols){welcomelen = E.screencols;};
 			int padding = (E.screencols - welcomelen) / 2;
-			
+		
 			if (padding) { abAppend(ab, "~", 1); padding--;}
 
 			while (padding--) {abAppend(ab, " ", 1); }
-	
+
 			abAppend(ab, welcome, welcomelen);
 
 		} else { abAppend(ab, "~", 1);}
-		
+	
 		abAppend(ab, "\x1b[K", 3);	
 		if (y < E.screenrows -1) {
-			abAppend(ab, "\r\n", 2);
+		abAppend(ab, "\r\n", 2);
 		}	
-	}
+	}	
 
 }
 
@@ -193,21 +219,35 @@ void editorMoveCursor(int key) {
 
 void editorProcessKeypress() {
 	int c = editorReadKey();
-	
+
 	switch(c) {
 		case CTRL_KEY('q'):
 			write(STDIN_FILENO, "\x1b[2J", 4);
 			write(STDIN_FILENO, "\x1b[H", 3);
 			exit(0);
 			break;
-
+		case HOME_KEY:
+			E.cx = 0;
+			break;
+		case END_KEY:
+			E.cx = E.screencols - 1;
+			break;
+		case PAGE_UP:
+		case PAGE_DOWN:
+			{
+				int times = E.screenrows;
+				while (times--) {
+					editorMoveCursor(c == PAGE_UP ? ARROW_UP : ARROW_DOWN);
+				}
+			}
+			break;
 		case ARROW_LEFT:
 		case ARROW_RIGHT:
 		case ARROW_DOWN:
 		case ARROW_UP:
 			editorMoveCursor(c);
-			break;
-	}
+		break;
+	}	
 }
 
 void editorRefreshScreen() {
@@ -224,7 +264,7 @@ void editorRefreshScreen() {
 
 	abAppend(&ab, "\x1b[H", 3);
 	abAppend(&ab, "\x1b[?25h", 6);
-	
+
 	write(STDIN_FILENO, ab.b, ab.len);
 
 	abFree(&ab);
@@ -238,13 +278,13 @@ void disableRawMode() {
 
 
 void enableRawMode() {
-	/*
-	Enables raw mode:
-		 pass in the original value of struct at the beginning and in other to reset the terminal prperly at exit
-	*/
+/*
+Enables raw mode:
+	 pass in the original value of struct at the beginning and in other to reset the terminal prperly at exit
+*/
 
 	if (tcgetattr(STDIN_FILENO, &E.orig_termios) == -1) die("tcgetattr");
-	
+
 	atexit(disableRawMode);
 
 	struct termios raw = E.orig_termios;
